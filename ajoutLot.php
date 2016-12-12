b7fcdc0080d63bcb6226bcebeded86250fb790c8
Reda M <maachi.reda@gmail.com>
2016-12-12 13:36:58 +0100

    First commit

---
+++ ajoutLot.php
@@ -0,0 +1,6 @@
+<?php
+   echo("First name: " . $_GET['inputNom'] . "<br />\n");
+   echo("Last name: " . $_GET['inputPrenom'] . "<br />\n");
+   echo("Last Phone: " . $_GET['inputTelephone'] . "<br />\n");
+   echo("Last Email: " . $_GET['inputEmail3'] . "<br />\n");
+?>
