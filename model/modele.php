047f80ad5d565caad7214ea7379f93ff417d9911
jeremybeule <jeremy.beule@live.fr>
2016-12-13 15:23:34 +0100

    Rajout de la classe Modele

---
+++ model/modele.php
@@ -0,0 +1,14 @@
+<?php
+
+//Cette classe représente les modèles des articles vendus par l'ecole saint Hilare
+
+class Modele
+{
+  private $_idModele;
+  private $_libelle;
+  private $_categorie;
+  private $_homologation;
+        
+  // Nous déclarons une méthode dont le seul but est d'afficher un texte.
+
+}
