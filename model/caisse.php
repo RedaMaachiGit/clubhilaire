e286afbbd1f40290d0687da0ba8f44104804509f
Reda M <maachi.reda@gmail.com>
2017-02-02 16:20:02 +0100

    a

--- model/caisse.php
+++ model/caisse.php
@@ -417,8 +417,8 @@ class Caisse
     }
 
     public static function getResultat(){
-      $fraisDeDepot = "Paiement de frais de dépôt COLLATE utf8_bin";
-      $query = "SELECT * FROM caisse WHERE typeTransaction =\"Paiement de frais de dépôt\" COLLATE utf8_bin";
+      $fraisDeDepot = mysql_real_escape_string(iconv('UTF-8', 'ASCII//TRANSLIT', "Paiement de frais de dépôt"));
+      $query = "SELECT * FROM caisse WHERE typeTransaction =". $fraisDeDepot;
       $query1 = "SELECT * FROM caisse WHERE typeTransaction = \"Vente de lot\"";
       $query2 = "SELECT * FROM caisse WHERE typeTransaction = \"Ouverture caisse\"";
       $db = new DB();
